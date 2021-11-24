7cca123f20d72e1c45eb97c9d701ab3b1f4385b6
Peter Dev <4706435+makafanpeter@users.noreply.github.com>
2021-11-24 00:42:17 +0000

    Worked on Image Rekognition Service

--- lambda/document-service-api/src/services/image-processing-service.ts
+++ lambda/document-service-api/src/services/image-processing-service.ts
@@ -1,5 +1,12 @@
 import {Rekognition} from "aws-sdk";
 import {FaceCriteria} from "../models/face-criteria";
+import {
+    CompareFacesRequest,
+    CompareFacesResponse,
+    DetectFacesRequest,
+    DetectFacesResponse
+} from "aws-sdk/clients/rekognition";
+import {DomainError} from "../models/errors/domain-error";
 
 class ImageProcessingService {
 
@@ -15,11 +22,42 @@ class ImageProcessingService {
     /**
      *
      * @param file
-     * @param criteria
+     * @param faceCriteria
      */
-    detectFace = async (file:Buffer, criteria:FaceCriteria): Promise<boolean> =>{
+    detectFace = async (file:Buffer, faceCriteria:FaceCriteria): Promise<boolean> =>{
 
-        return true;
+        const params: DetectFacesRequest = {
+            Image:{
+                Bytes : file
+            },
+            Attributes:["ALL"]
+        };
+       const response: DetectFacesResponse   = await this.rekognition.detectFaces(params).promise().catch(error => {
+            throw new DomainError("SYSTEM_ERROR", error.message);
+        });
+
+       let result = false;
+       response.FaceDetails?.forEach((face) => {
+
+           if (faceCriteria.eyeGlasses)
+           {
+               result = face.Eyeglasses == faceCriteria.eyeGlasses;
+           }
+           if (faceCriteria.eyesOpen)
+           {
+               result = face.EyesOpen == faceCriteria.eyesOpen;
+           }
+           if (faceCriteria.mouthOpen)
+           {
+               result = face.MouthOpen == faceCriteria.mouthOpen;
+           }
+           if (faceCriteria.sunGlasses)
+           {
+               result = face.Sunglasses == faceCriteria.sunGlasses;
+           }
+       });
+
+        return result;
     }
 
     /**
@@ -29,8 +67,25 @@ class ImageProcessingService {
      */
     compareFace = async (source: Buffer, target:Buffer):Promise<boolean> =>{
 
+        let result = false;
+        const params: CompareFacesRequest = {
+            SimilarityThreshold: 50,
+            SourceImage: {
+                Bytes : source
+            },
+            TargetImage: {
+                Bytes: target
+            }
+        }
 
-        return true;
+        const response: CompareFacesResponse   = await this.rekognition.compareFaces(params).promise().catch(error => {
+            throw new DomainError("SYSTEM_ERROR", error.message);
+        });
+
+        if (response.FaceMatches){
+                result = true;
+        }
+        return result;
     }
 }
 
